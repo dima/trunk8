4f5ba6cf2801fc947fb52e8669ee04e1d3f38ede
Dima Berastau <dima.berastau@gmail.com>
2013-05-17 11:14:27 -0700

    If the string length is less than explicitly specified width just leave the string intact.

--- trunk8.js
+++ trunk8.js
@@ -1,10 +1,10 @@
 /**!
  * trunk8 v1.3.1
  * https://github.com/rviscomi/trunk8
- * 
+ *
  * Copyright 2012 Rick Viscomi
  * Released under the MIT License.
- * 
+ *
  * Date: September 26, 2012
  */
 
@@ -22,13 +22,13 @@
 		WIDTH = {
 			auto: 'auto'
 		};
-	
+
 	function trunk8(element) {
 		this.$element = $(element);
 		this.original_text = this.$element.html();
 		this.settings = $.extend({}, $.fn.trunk8.defaults);
 	}
-	
+
 	trunk8.prototype.updateSettings = function (options) {
 		this.settings = $.extend(this.settings, options);
 	};
@@ -46,7 +46,7 @@
 			lower, upper,
 			bite_size,
 			bite;
-		
+
 		/* Reset the field to the original string. */
 		this.html(str);
 
@@ -64,9 +64,9 @@
 
 			while (lower <= upper) {
 				bite_size = lower + ((upper - lower) >> 1);
-				
+
 				bite = utils.eatStr(str, side, length - bite_size, fill);
-				
+
 				this.html(bite);
 
 				/* Check for overflow. */
@@ -83,10 +83,10 @@
 
 			/* Reset the content to eliminate possible existing scroll bars. */
 			this.html('');
-			
+
 			/* Display the biggest bite. */
 			this.html(max_bite);
-			
+
 			if (settings.tooltip) {
 				this.attr('title', str);
 			}
@@ -94,10 +94,14 @@
 		else if (!isNaN(width)) {
 			bite_size = length - width;
 
+      if (bite_size <= 0) {
+        bite_size = 0;
+      }
+
 			bite = utils.eatStr(str, side, bite_size, fill);
 
 			this.html(bite);
-			
+
 			if (settings.tooltip) {
 				this.attr('title', str);
 			}
@@ -112,13 +116,13 @@
 			return this.each(function () {
 				var $this = $(this),
 					data = $this.data('trunk8');
-				
+
 				if (!data) {
 					$this.data('trunk8', (data = new trunk8(this)));
 				}
-				
+
 				data.updateSettings(options);
-				
+
 				truncate.call($this);
 			});
 		},
@@ -127,7 +131,7 @@
 		update: function (new_string) {
 			return this.each(function () {
 				var $this = $(this);
-				
+
 				/* Update text. */
 				if (new_string) {
 					$this.data('trunk8').original_text = new_string;
@@ -137,12 +141,12 @@
 				truncate.call($this);
 			});
 		},
-		
+
 		revert: function () {
 			return this.each(function () {
 				/* Get original text. */
 				var text = $(this).data('trunk8').original_text;
-				
+
 				/* Revert element to original text. */
 				$(this).html(text);
 			});
@@ -166,7 +170,7 @@
 	        if (utils.eatStr.cache[key]) {
 				return utils.eatStr.cache[key];
 	        }
-	        
+
 			/* Common error handling. */
 		    if ((typeof str !== 'string') || (length === 0)) {
 		    	$.error('Invalid source string "' + str + '".');
@@ -188,12 +192,12 @@
 			        /* str... */
 		            return utils.eatStr.cache[key] =
 			            	$.trim(str.substr(0, length - bite_size)) + fill;
-		            
+
 		        case SIDES.left:
 			        /* ...str */
 		            return utils.eatStr.cache[key] =
 			            	fill + $.trim(str.substr(bite_size));
-		            
+
 		        case SIDES.center:
 			        /* Bit-shift to the right by one === Math.floor(x / 2) */
 		            half_length = length >> 1; // halve the length
@@ -204,12 +208,12 @@
 			            	$.trim(utils.eatStr(str.substr(0, length - half_length), SIDES.right, bite_size - half_bite_size, '')) +
 		            		fill +
 		            		$.trim(utils.eatStr(str.substr(length - half_length), SIDES.left, half_bite_size, ''));
-		            
+
 		        default:
 		        	$.error('Invalid side "' + side + '".');
 		    }
 		},
-		
+
 		getLineHeight: function (elem) {
 	            var floats = $(elem).css('float');
 	            if (floats !== 'none') {
@@ -219,20 +223,20 @@
 	            if (pos === 'absolute') {
 	                $(elem).css('position', 'static');
 	            }
-	
+
 	            var html = $(elem).html(),
 		        wrapper_id = 'line-height-test',
 		        line_height;
-	
+
 	            /* Set the content to a small single character and wrap. */
 	            $(elem).html('i').wrap('<div id="' + wrapper_id + '" />');
-	
+
 	            /* Calculate the line height by measuring the wrapper.*/
 	            line_height = $('#' + wrapper_id).innerHeight();
-	
+
 	            /* Remove the wrapper and reset the content. */
 	            $(elem).html(html).css({ 'float': floats, 'position': pos }).unwrap();
-	
+
 	            return line_height;
 	        }
 	};
@@ -241,7 +245,7 @@
 	utils.eatStr.generateKey = function () {
 		return Array.prototype.join.call(arguments, '');
 	};
-	
+
 	$.fn.trunk8 = function (method) {
 		if (methods[method]) {
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -253,7 +257,7 @@
 			$.error('Method ' + method + ' does not exist on jQuery.trunk8');
 		}
 	};
-	
+
 	/* Default trunk8 settings. */
 	$.fn.trunk8.defaults = {
 		fill: '&hellip;',
